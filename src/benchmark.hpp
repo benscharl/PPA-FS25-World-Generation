af7d87fcd772d2fbb90966be4c4be080efed35c9
benscharl <ben.scharlemann@gmail.com>
2026-04-24 18:42:47 -0400

    Remove benchmark for deleted tree placement

--- src/benchmark.hpp
+++ src/benchmark.hpp
@@ -31,15 +31,6 @@ bool test_correctness(const HeightMap& ref, const HeightMap& candidate,
   return true;
 }
 
-bool compare_tree_locations(const std::vector<glm::vec2>& a,
-                            const std::vector<glm::vec2>& b) {
-  if (a.size() != b.size()) return false;
-  for (size_t i = 0; i < a.size(); i++) {
-    if (a[i] != b[i]) return false;
-  }
-  return true;
-}
-
 void warmup() {
   std::cout << "Perlin generation only:" << std::endl;
   std::cout << "Warming up for 3 seconds..." << std::endl;
@@ -93,46 +84,6 @@ inline HeightMap benchmark_heightmap_hybrid(MapGenerator& mapGenerator,
   });
 }
 
-/*
-inline std::vector<glm::vec2> benchmark_tree_generation(
-    const bool use_parallel, CMDSettings& settings,
-    const TreePlacementConfig& treeConfig = TreePlacementConfig{},
-    const HeightmapConfig& heightmapConfig = HeightmapConfig{}) {
-  PerlinNoise* perlinNoise =
-      get_perlin_noise_generator(false, heightmapConfig.seed);
-  std::vector<std::vector<double>> heightmap;
-  heightmap.assign(settings.dimension,
-                   std::vector<double>(settings.dimension, 0.0));
-
-  perlinNoise->generate_normalized_heightmap(
-      heightmapConfig.octaves, heightmapConfig.frequency,
-      glm::vec2(settings.dimension, settings.dimension), heightmap);
-
-  if (use_parallel)
-    std::cout << "Starting parallel tree placement benchmark" << std::endl;
-  if (!use_parallel)
-    std::cout << "Starting sequential tree placement benchmark" << std::endl;
-
-  std::vector<std::chrono::duration<double>> times;
-  for (int r = 0; r < BENCHMARK_QUANTITY; ++r) {
-    auto start = std::chrono::high_resolution_clock::now();
-    place_trees(heightmap, use_parallel, treeConfig, heightmapConfig);
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = stop - start;
-    std::cout << "Time " << diff.count() << std::endl;
-    times.push_back(diff);
-  }
-
-  std::chrono::duration<double> total_time = std::accumulate(
-      times.begin(), times.end(), std::chrono::duration<double>(0.0));
-  std::cout << "average " << (total_time / times.size()).count() << std::endl;
-  std::vector<glm::vec2> tree_placement =
-      place_trees(heightmap, use_parallel, treeConfig, heightmapConfig);
-
-  return tree_placement;
-};
-*/
 void benchmark(CMDSettings& settings) {
   PerlinNoise perlinNoise(settings.seed);
   HeightmapConfig config{settings.dimension, settings.dimension};
